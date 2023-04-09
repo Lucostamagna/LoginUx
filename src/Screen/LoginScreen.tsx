5b84a95af80f6810aea6f3d5ef7bb90ca6b5b832
LUCIA <luci3220@hotmail.com>
2023-04-09 00:44:16 -0300

    styles

--- src/Screen/LoginScreen.tsx
+++ src/Screen/LoginScreen.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { View, Text, Dimensions, Image } from 'react-native';
-import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
-import { useForm } from '../Hooks/useForm';
-import LottieView from 'lottie-react-native';
-import Animated, { FadeInDown, FadeInRight } from 'react-native-reanimated';
-
+import React from "react";
+import { View, Text, Dimensions, Image } from "react-native";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { useForm } from "../Hooks/useForm";
+import LottieView from "lottie-react-native";
+import { Feather } from "@expo/vector-icons";
+import Animated, { FadeInDown, FadeInRight } from "react-native-reanimated";
 
 const LoginScreen = () => {
-    const { width, height } = Dimensions.get('window')
+  const { width, height } = Dimensions.get("window");
   //onchange me va a permitir cambiar los datos del formulario
   const { email, password, onChange } = useForm({
     email: "",
@@ -44,19 +44,155 @@ const LoginScreen = () => {
         </Text>
       </View>
       <>
+        {/* _______________________ */}
+        <Image
+          source={require("../Screen/save.png")}
+          style={{
+            width: 175,
+            height: 175,
+            resizeMode: "contain",
+            alignSelf: "center",
+            marginTop: 20,
+          }}
+        />
 
-    <Animated.View>  
-<Image source={require('../Screen/save.png')} style={{ width: 175, height: 175, resizeMode: 'contain', alignSelf: 'center', marginTop: 20 }} />
+        <Text
+          style={{
+            fontSize: 30,
+            color: "#323646",
+            alignSelf: "center",
+            marginTop: 20,
+          }}
+        >
+          Welcome to Todify!
+        </Text>
+        <Text style={{ fontSize: 16, color: "#323646", alignSelf: "center" }}>
+          Make your day great!
+        </Text>
+        {/* ------------------ */}
 
-<Text style={{  fontSize: 30, color: '#323646', alignSelf: 'center', marginTop: 20 }}>Welcome to Todify!</Text>
- <Text style={{  fontSize: 16, color: '#323646', alignSelf: 'center' }}>Make your day great!</Text>
- </Animated.View> 
+        {/* --ANMIATED */}
+        <Text
+          style={{
+            fontFamily: "Poppins-Regular",
+            fontSize: 16,
+            color: "#323646",
+            marginTop: 20,
+          }}
+        >
+          Email
+        </Text>
+        <View
+          style={{
+            backgroundColor: "#f6f6f6",
+            height: 50,
+            borderRadius: 10,
+            paddingHorizontal: 10,
+          }}
+        >
+          <TextInput
+            value="hi@reactnativespace.com"
+            style={{
+              // fontFamily: "Poppins-Medium",
+              color: "#323646",
+              padding: 10,
+            }}
+          />
+        </View>
 
+        <Text
+          style={{
+            // fontFamily: "Poppins-Regular",
+            fontSize: 16,
+            color: "#323646",
+            marginTop: 20,
+          }}
+        >
+          Password
+        </Text>
+        <View
+          style={{
+            backgroundColor: "#f6f6f6",
+            height: 50,
+            borderRadius: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            paddingHorizontal: 10,
+          }}
+        >
+          <TextInput
+            value="1234567890"
+            secureTextEntry={true}
+            style={{
+              // fontFamily: "Poppins-Medium",
+              color: "#323646",
+              padding: 10,
+            }}
+          />
+          <Feather name="eye" size={24} color="#323646" />
+        </View>
+
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#50e3c2",
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center",
+            height: 50,
+            marginTop: 20,
+          }}
+        >
+          <Text
+            style={{
+              // fontFamily: "Poppins-Medium",
+              fontSize: 16,
+              color: "#FFF",
+            }}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            // fontFamily: "Poppins-Medium",
+            fontSize: 14,
+            color: "#50e3c2",
+            padding: 10,
+            textAlign: "center",
+          }}
+        >
+          Forgot password?
+        </Text>
+
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            alignSelf: "center",
+            alignItems: "flex-end",
+          }}
+        >
+          <Text
+            style={{
+              // fontFamily: "Poppins-Medium",
+              fontSize: 14,
+              color: "#323646",
+            }}
+          >
+            Don't have an account?{" "}
+          </Text>
+          <Text
+            style={{
+              // fontFamily: "Poppins-Medium",
+              fontSize: 14,
+              color: "#50e3c2",
+            }}
+          >
+            Register!
+          </Text>
+        </TouchableOpacity>
       </>
-
-      
-      
-      
 
       {/* <Text> EMAIL</Text>
       <TextInput
