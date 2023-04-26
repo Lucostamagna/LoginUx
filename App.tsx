022a6a54148c0221305053ea1886bacc08c029dc
LUCIA <luci3220@hotmail.com>
2023-04-25 23:07:19 -0300

    conection provider and app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/Navigator/Navigator';
+import { AuthProvider } from './src/Context/AuthContext';
+
+// de esta manera conecto mi authprovider con mi app global
+const AppState=({children}: any)=>{
+  return(
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  )
+}
+
+
 
 
 export default function App() {
   return (
     <NavigationContainer>
+      <AppState>
       <Navigator/>
+      </AppState>
     </NavigationContainer>
   )
 }
--- src/Context/AuthContext.tsx
+++ src/Context/AuthContext.tsx
@@ -1,12 +1,30 @@
 import React, {createContext} from 'react'
+import { Usuario } from '../Interfaces/AppInterface';
 
 //que informacion quiero que este disponible para toda mi app?
 //uso type y no interface por que no voy a expandir
-type AuthContectProps={
+type AuthContextProps={
 errorMessage:string; //cuando sucesa algun error puedo almacenarlo aqui
 token:string|null;
- //respuesta del backend                                                                                          
-
+ user:Usuario;//regspuesta del backend                                                                                          
+status: 'checking' | 'authenticated' | 'not-authenticated'
+//estados de mis usuarios, estados personalizados 
+//funciones para ingresar, salir, etc
+singUp:()=>void;
+singIn:()=>void;
+singOut:()=>void;
+logOut:()=>void;
+removeError:()=>void;
 }
 
-const AuthContext = createContext({});
+export const AuthContext = createContext({} as AuthContextProps ); //este es mi context. le indique como debe comportarse
+
+export const AuthProvider=({children}:any)=>{
+return(
+    <AuthContext.Provider value={{
+
+    }}>
+        {children}
+    </AuthContext.Provider>
+)
+}
