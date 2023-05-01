aa486689dc5fea16f9a26d39b7f75e489f6a14f9
LUCIA <luci3220@hotmail.com>
2023-05-01 20:05:42 -0300

    add useReducer

--- src/Context/AuthContext.tsx
+++ src/Context/AuthContext.tsx
@@ -1,12 +1,13 @@
-import React, {createContext} from 'react'
+import React, {createContext, useReducer} from 'react'
 import { Usuario } from '../Interfaces/AppInterface';
+import { authReducer, AuthState } from './AuthReducer';
 
 //que informacion quiero que este disponible para toda mi app?
 //uso type y no interface por que no voy a expandir
 type AuthContextProps={
 errorMessage:string; //cuando sucesa algun error puedo almacenarlo aqui
 token:string|null;
- user:Usuario;//regspuesta del backend                                                                                          
+ user:Usuario |null ;//respuesta del backend                                                                                          
 status: 'checking' | 'authenticated' | 'not-authenticated'
 //estados de mis usuarios, estados personalizados 
 //funciones para ingresar, salir, etc
@@ -17,14 +18,48 @@ logOut:()=>void;
 removeError:()=>void;
 }
 
+
+
+
+const aunthInitialState: AuthState ={
+    status:'checking', //revisando el token
+    token: null,
+    user:null,
+    errorMessage:''
+}
+
 export const AuthContext = createContext({} as AuthContextProps ); //este es mi context. le indique como debe comportarse
 
 export const AuthProvider=({children}:any)=>{
-return(
-    <AuthContext.Provider value={{
 
-    }}>
-        {children}
-    </AuthContext.Provider>
-)
+
+
+    //estado inicial cuando se ingresa a la app
+    const [state, dispatch] =useReducer(authReducer, aunthInitialState);
+
+     const singUp = ()=>{}
+    const singIn=()=>{}
+    const singOut=()=>{}
+     const logOut=()=>{}
+    const removeError=()=>{}
+
+
+
+
+
+
+return (
+  <AuthContext.Provider
+    value={{
+      ...state,
+      singUp,
+      singIn,
+      singOut,
+      logOut,
+      removeError,
+    }}
+  >
+    {children}
+  </AuthContext.Provider>
+);
 }
--- src/Context/AuthReducer.tsx
+++ src/Context/AuthReducer.tsx
@@ -1,66 +1,60 @@
 import { Usuario } from "../Interfaces/AppInterface";
 
-
-
-
-
-
 //estado inicial
 export interface AuthState {
-    status: 'checking' | 'authenticated' | 'not-authenticated';
-    errorMessage:string;
-    token:string|null;
-    user:Usuario  |null;
+  status: "checking" | "authenticated" | "not-authenticated";
+  errorMessage: string;
+  token: string | null;
+  user: Usuario | null;
 }
-type AuthAction=
-| {type:'signUp', payload:{token: string, user:Usuario} }
-|{type:'addError', payload: string}
-|{type:'removeError'}
-|{type:'notAuthenticated'} //si  el token falla, esta accion limpia mi estado y lo deja como el inicial
-|{type: 'logout'} //cerrar sesion
+type AuthAction =
+  | { type: "signUp"; payload: { token: string; user: Usuario } }
+  | { type: "addError"; payload: string }
+  | { type: "removeError" }
+  | { type: "notAuthenticated" } //si  el token falla, esta accion limpia mi estado y lo deja como el inicial
+  | { type: "logout" }; //cerrar sesion
 
 //CREO MI REDUCER
 
-export const authReducer= (state: AuthState, action: AuthAction) : AuthState=>{
-switch (action.type) {
-    case 'addError':
-        return {
-            ...state,
-            user:null, //si hay un error el usuario es null
-            status:'not-authenticated', //si hay un erro no va a estar autenticado
-            token: null,
-            errorMessage: action.payload,
-            
-
-        }
-        case 'removeError': {
-            return {
-                ...state,
-              errorMessage: ''  
-
-            }
-        }
-        case 'signUp': {
-            return {
-                ...state,
-                errorMessage:'',
-                status: 'authenticated',
-                token: action.payload.token,
-                user: action.payload.user
-            }
-
-        }
-        case 'logout': //estos dos casos hacen lo mismo
-        case 'notAuthenticated':{
-            return {
-                ...state,
-                status:'not-authenticated',
-                token:null,
-                user: null,
-            }
-        }
-       
-        default:
-            return state;
+export const authReducer = (
+  state: AuthState,
+  action: AuthAction
+): AuthState => {
+  switch (action.type) {
+    case "addError":
+      return {
+        ...state,
+        user: null, //si hay un error el usuario es null
+        status: "not-authenticated", //si hay un erro no va a estar autenticado
+        token: null,
+        errorMessage: action.payload,
+      };
+    case "removeError": {
+      return {
+        ...state,
+        errorMessage: "",
+      };
     }
-}
+    case "signUp": {
+      return {
+        ...state,
+        errorMessage: "",
+        status: "authenticated",
+        token: action.payload.token,
+        user: action.payload.user,
+      };
+    }
+    case "logout": //estos dos casos hacen lo mismo
+    case "notAuthenticated": {
+      return {
+        ...state,
+        status: "not-authenticated",
+        token: null,
+        user: null,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
