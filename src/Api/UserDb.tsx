87d672c236d066f00f96e53eeaee931ba6137cd0
Lucía Costamagna <luci3220@hotmail.com>
2023-06-19 20:35:49 -0300

    asyncStorage

--- src/Api/UserDb.tsx
+++ src/Api/UserDb.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 //endpoint sobre el cual realizo las peticiones
 //ip de mi equipo
@@ -7,6 +9,15 @@ const baseURL= 'http://192.168.120.56:8080/api';
 //peticiones http
 const userDB = axios.create({ baseURL});
 
+userDB.interceptors.request.use(
+    async(config)=>{
+        const token= await AsyncStorage.getItem('token');
+        if(token) {
+            config.headers["x-token"]= token;
+        }
+return config;
+    }
+)
 
 
 
--- src/Context/AuthContext.tsx
+++ src/Context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { LoginData, LoginResponse, Usuario } from "../Interfaces/AppInterface";
 import { authReducer, AuthState } from "./AuthReducer";
 import userDB from "../Api/UserDb";
@@ -23,31 +24,55 @@ const aunthInitialState: AuthState = {
   errorMessage: "",
 };
 
-export const AuthContext = createContext({} as AuthContextProps); 
+export const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider = ({ children }: any) => {
-
   const [state, dispatch] = useReducer(authReducer, aunthInitialState);
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const checkToken = async () => {
+    const token = await AsyncStorage.getItem("token");
+    //no token, no autenticado
+    if (!token) return dispatch({ type: "notAuthenticated" });
+    //hay token,
+    const resp = await userDB.get("/auth");
+    if (resp.status !== 200) {
+      return dispatch({ type: "notAuthenticated" });
+    }
+
+    dispatch({
+      type: "signUp",
+      payload: {
+        token: resp.data.token,
+        user: resp.data.usuario,
+      },
+    });
+  };
 
   const singIn = async ({ correo, password }: LoginData) => {
     try {
-      const {data} = await userDB.post<LoginResponse>("/auth/login", {
+      const { data } = await userDB.post<LoginResponse>("/auth/login", {
         correo,
         password,
       });
-     dispatch({
-      type:'signUp',
-      payload :{
-        token: data.token,
-        user:data.usuario
-      }
-     })
-    } catch(error) {
-      console.log({error})
+      dispatch({
+        type: "signUp",
+        payload: {
+          token: data.token,
+          user: data.usuario,
+        },
+      });
+
+      await AsyncStorage.setItem("token", data.token);
+    } catch (error) {
+      console.log({ error });
       dispatch({
         type: "addError",
-        payload:'informacion incorrecta'
-      })
+        payload: "informacion incorrecta",
+      });
     }
   };
 
@@ -56,8 +81,8 @@ export const AuthProvider = ({ children }: any) => {
   const logOut = () => {};
   const removeError = () => {
     dispatch({
-      type:'removeError'
-    })
+      type: "removeError",
+    });
   };
 
   return (
