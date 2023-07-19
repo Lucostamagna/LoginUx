665aee164c76fefc169825392a22bc9a77252853
Lucía Costamagna <luci3220@hotmail.com>
2023-07-19 16:17:08 -0300

    create ProductContext

--- App.tsx
+++ App.tsx
@@ -2,13 +2,17 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/Navigator/Navigator';
 import { AuthProvider } from './src/Context/AuthContext';
+import { ProductProvider } from './src/Context/ProductContext';
 
 
 // de esta manera conecto mi authprovider con mi app global
 const AppState=({children}: any)=>{
   return(
+    
     <AuthProvider>
+      <ProductProvider>
       {children}
+      </ProductProvider>
     </AuthProvider>
   )
 }
---
+++ src/Context/ProductContext.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useState } from "react";
+import { Producto } from "../Interfaces/AppInterface";
+
+type ProductContextProps = {
+  product: Producto[];
+  loadProduct: () => Promise<void>;
+  addProduct: (categoryId: string, productName: string) => Promise<void>;
+  updateProduct: (
+    categoryId: string,
+    productName: string,
+    productId: string
+  ) => Promise<void>;
+  deleteProduct: (id: string) => Promise<void>;
+  loadProductById: (id: string) => Promise<Producto>;
+  uploadImage: (data: any, id: string) => Promise<void>;
+};
+
+export const ProductContext = createContext({} as ProductContextProps);
+
+const [product, setProduct] = useState<Producto[]>([]);
+
+const loadProduct = async () => {};
+const addProduct = async (categoryId: string, productName: string) => {};
+const updateProduct = async (
+  categoryId: string,
+  productName: string,
+  productId: string
+) => {};
+const deleteProduct = async (id: string) => {};
+const loadProductById = async (id: string) => {
+  throw new Error("not implemented");
+};
+const uploadImage = async (data: any, id: string) => {};
+
+export const ProductProvider = ({ children }: any) => {
+  return (
+    <ProductContext.Provider
+      value={{
+        product,
+        loadProduct,
+        addProduct,
+        updateProduct,
+        deleteProduct,
+        loadProductById,
+        uploadImage,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
